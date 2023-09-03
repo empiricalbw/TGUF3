3c1e7b7dfb19c90ae0a7f2fdf2fc0c22d57358fc
Empirical <empirical.bw@gmail.com>
2023-09-02 21:12:41 -0600

    TG3AuraList.lua: Renames sub-frames array from "Frames" to "children".

--- TG3AuraList.lua
+++ TG3AuraList.lua
@@ -10,7 +10,7 @@ function TGUF3.AuraList:Init(elem, xml)
     local dy        = elem.dy or 0
     local dimension = self:GetHeight()
 
-    self.Frames = {}
+    self.children = {}
     for i=1, count do
         local f = CreateFrame("Frame", nil, self, xml)
         f:SetWidth(dimension)
@@ -18,14 +18,14 @@ function TGUF3.AuraList:Init(elem, xml)
         if i == 1 then
             f:SetPoint(elem.align.."LEFT", self, elem.align.."LEFT", 0, 0)
         else
-            f:SetPoint(elem.align.."LEFT", self.Frames[i-1],
+            f:SetPoint(elem.align.."LEFT", self.children[i-1],
                        elem.align.."RIGHT", dx, dy)
         end
         f.unitFrame = self.unitFrame
         f:Init({index = i, dimension = dimension,
                 mineDimension = elem.mineDimension})
         self.unitFrame.unit:AddListener(f)
-        self.Frames[i] = f
+        self.children[i] = f
     end
 end
 
