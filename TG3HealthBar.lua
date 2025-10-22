5bbca5022d5ace3a1a83f78f995f099e3dba33f6
Empirical <empirical.bw@gmail.com>
2025-10-22 10:13:00 -0600

    TG3HealthBar: Add support for incoming heals.

--- TG3HealthBar.lua
+++ TG3HealthBar.lua
@@ -25,7 +25,6 @@
 TGUF3.HealthBar = {
     _name  = "TGUF3.HealthBar",
     _xml   = "TG3HealthBarTemplate",
-    _ftype = "StatusBar",
 }
 TG3HealthBarMixin = TGUF3.HealthBar
 
@@ -42,12 +41,55 @@ function TGUF3.HealthBar:Init(elem)
     self.color       = elem.color       or {0, 1, 0, 1}
     self.highColor   = elem.highColor   or {1, 1, 0, 1}
     self.lowColor    = elem.lowColor    or {1, 0, 0, 1}
+    self.icolor      = elem.icolor      or {unpack(self.color)}
+    self.ihighColor  = elem.ihighColor  or {unpack(self.highColor)}
+    self.ilowColor   = elem.ilowColor   or {unpack(self.lowColor)}
     self.tappedColor = elem.tappedColor or {0.5, 0.5, 0.5, 1}
     self.highPercent = (elem.highPercent or 60) / 100
     self.lowPercent  = (elem.lowPercent  or 30) / 100
 
+    local ialpha       = elem.ialpha      or 0.67
+    self.icolor[4]     = ialpha
+    self.ihighColor[4] = ialpha
+    self.ilowColor[4]  = ialpha
+
+    self.realWidth = self:GetWidth()
+
     local texture = elem.texture or "Interface/Addons/TGUF3/DUF_Images/bg"
-    self:SetStatusBarTexture(texture)
+    self.BackBar.Texture:SetTexture(texture)
+    self.BackBar:Hide()
+    self.FrontBar.Texture:SetTexture(texture)
+    self.FrontBar:Hide()
+end
+
+function TGUF3.HealthBar:SetBar(bar, tapped, health, maxHealth, highColor,
+        lowColor, color)
+    local p = health / maxHealth
+    local pw = math.floor(p * self.realWidth + 0.5)
+    if pw <= 0 then
+        pw = 1
+    end
+    bar:SetWidth(pw)
+
+    if tapped then
+        bar.Texture:SetVertexColor(unpack(self.tappedColor))
+    elseif self.colorize then
+        if p <= self.lowPercent then
+            bar.Texture:SetVertexColor(unpack(lowColor))
+        elseif p <= self.highPercent then
+            p = (p - self.lowPercent)/(self.highPercent - self.lowPercent)
+            bar.Texture:SetVertexColor(interp(p, lowColor, highColor))
+        elseif p < 1 then
+            p = (p - self.highPercent)/(1 - self.highPercent)
+            bar.Texture:SetVertexColor(interp(p, highColor, color))
+        else
+            bar.Texture:SetVertexColor(unpack(color))
+        end
+    else
+        bar.Texture:SetVertexColor(unpack(color))
+    end
+
+    bar:Show()
 end
 
 function TGUF3.HealthBar:UPDATE_HEALTH(unit)
@@ -55,26 +97,23 @@ function TGUF3.HealthBar:UPDATE_HEALTH(unit)
         return
     end
 
-    if unit.tapped then
-        self:SetStatusBarColor(unpack(self.tappedColor))
-    elseif self.colorize then
-        local p = unit.health.current / unit.health.max
-        if p <= self.lowPercent then
-            self:SetStatusBarColor(unpack(self.lowColor))
-        elseif p <= self.highPercent then
-            p = (p - self.lowPercent)/(self.highPercent - self.lowPercent)
-            self:SetStatusBarColor(interp(p, self.lowColor, self.highColor))
-        elseif p < 1 then
-            p = (p - self.highPercent)/(1 - self.highPercent)
-            self:SetStatusBarColor(interp(p, self.highColor, self.color))
-        else
-            self:SetStatusBarColor(unpack(self.color))
-        end
+    local incomingHeals = unit.incomingHeals or 0
+    local incomingHealth = unit.health.current + incomingHeals
+    local maxHealth = unit.health.max
+    incomingHealth = math.min(incomingHealth, maxHealth)
+
+    -- Back bar is the incoming heals bar.
+    if incomingHeals ~= 0 then
+        self:SetBar(self.BackBar, unit.tapped, incomingHealth, maxHealth,
+                    self.ihighColor, self.ilowColor, self.icolor)
     else
-        self:SetStatusBarColor(unpack(self.color))
+        self.BackBar:Hide()
     end
-    self:SetMinMaxValues(0, unit.health.max)
-    self:SetValue(unit.health.current)
+
+    -- Front bar is the actual health bar.
+    self:SetBar(self.FrontBar, unit.tapped, unit.health.current,
+                maxHealth, self.highColor, self.lowColor, self.color)
 end
 
 TGUF3.HealthBar.UPDATE_TAPPED = TGUF3.HealthBar.UPDATE_HEALTH
+TGUF3.HealthBar.UPDATE_INCOMINGHEALS = TGUF3.HealthBar.UPDATE_HEALTH
